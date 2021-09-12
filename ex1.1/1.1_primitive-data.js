d2b8b7f0f182fba237c4d1bba9b4ada0f7649273
wajdehbaish <wajdehbaish@gmail.com>
2021-09-12 13:27:25 +0300

    ex1.1

---
+++ ex1.1/1.1_primitive-data.js
@@ -0,0 +1,22 @@
+let str = 'hello world';
+console.log(str);
+
+let x = 5;
+let y = 6;
+let z = 5 + 6;
+console.log(z)
+
+const car = new Object();
+car.type = "bmw";
+car.color = "white";
+console.log(car);
+
+let isball = true;
+isball;
+console.log(isball);
+
+const size = undefined;
+console.log(size);
+
+let y = null;
+console.log(y);
