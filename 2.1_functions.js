c3f543d7650e6044d448c7e9ab963bc4419ef44c
wajdehbaish <wajdehbaish@gmail.com>
2021-09-12 15:20:44 +0300

    2.1

---
+++ 2.1_functions.js
@@ -0,0 +1,15 @@
+function describeCountry(country, population, capitalCity)
+
+{
+
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+
+
+}
+
+
+
+
+console.log(describeCountry('Spain', '100', 'Madrid'));
+console.log(describeCountry('France', '300', 'Paris'));
+console.log(describeCountry('Italy', '150', 'Roma'));
---
+++ 2.4_date.js
@@ -0,0 +1,23 @@
+let date = new Date();
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];​
+function getDayName() {
+    return days[date.getDay()]
+}​
+function getDayOfMonth() {
+    return date.getDate()
+}​
+function getMonthName() {
+    return monthNames[date.getMonth()];
+}​
+getMonthName()​
+function getYear() {
+    return date.getFullYear();
+}​
+//`Today is Sunday the 31 of January, 2021’
+function getDateFormat() {
+    return `Today is ${getDayName()} the ${getDayOfMonth()} of ${getMonthName()}, ${getYear()}`
+}​
+console.log(getDateFormat())
