f4e6769572a9a501027c8f9aa7520c9b90facefe
wajdehbaish <wajdehbaish@gmail.com>
2021-09-12 15:40:05 +0300

    2.2

---
+++ 2.2_furtune.js
@@ -0,0 +1,7 @@
+function tellFortune(jobTitle, location, partnerName, numberOfChildren)
+
+{
+    return `yo will be a${jobTitle} in ${location} and married to ${partnerName} with ${numberOfChildren} children`;
+}
+
+console.log(tellFortune('programmer', 'Tel aviv', 'sharon', '2'));
