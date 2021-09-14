277564a57c9a80332506d0222be92e71791092c2
wajdehbaish <wajdehbaish@gmail.com>
2021-09-14 14:30:14 +0300

    7.4

--- 7.4_Candy Store.js
+++ 7.4_Candy Store.js
@@ -33,17 +33,23 @@ function getPrice(candyStore, id) {
     let candiesArray = candyStore.candies
     for (let i = 0; i < candiesArray.length; i++) {
         if (candiesArray[i].id == id) {
-            return candiesArray[i]
+            return candiesArray[i].price
         }
     }
 }
-let x = getCandy(candyStore, "as12f")
-console.log(x);
+// let x = getPrice(candyStore, "as12f")
+// console.log(x);
 
 function addCandy(candyStore, id, name, price) {
-    //your code 
+    candyStore.candies.push({ id: id, name: name, price: price, amount: 1 })
 }
 
 function buy(candyStore, id) {
-    //your code 
+    let candiesArray = candyStore.candies
+    for (let i = 0; i < candiesArray.length; i++) {
+        if (candiesArray[i].id == id) {
+            candiesArray[i].amount = candiesArray[i].amount - 1
+            candiesArray[i].cashRegister = candiesArray[i].cashRegister + candiesArray[i].price
+        }
+    }
 }
