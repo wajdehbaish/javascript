1a60e5ff21f47e8ae889a5bc78f855cfa0a11cf7
wajdehbaish <wajdehbaish@gmail.com>
2021-09-15 16:55:12 +0300

    7.5

--- 7.5_count the letters.js
+++ 7.5_count the letters.js
@@ -1,3 +1,27 @@
-const array = [{
-            "Hello" + "Good Day" + "Your Welcome" + "hotdog" +
-            "hamburgers"];
+const array = ["Hello", "Good Day", "Your Welcome", "hotdog",
+    "hamburgers"
+];
+
+let count = arr => {
+    let temp = {}
+    arr.forEach(val => {
+
+
+        val = val.toLowerCase();
+        for (i = 0; i < val.length; i++) {
+            if (val[i] !== " ") {
+                if (temp[val[i]] === undefined)
+                    temp[val[i]] = 1;
+                else
+                    temp[val[i]]++;
+            }
+        }
+    });
+    max = Object.keys(temp)[0];
+    for (let val in temp) {
+        if (temp[max] < temp[val])
+            max = val;
+    }
+    return ([temp, max]);
+}
+console.log(count(array));
