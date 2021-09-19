f512127512c6d423997b9ceac35e3c1bcb99f375
wajdehbaish <wajdehbaish@gmail.com>
2021-09-19 13:02:01 +0300

    9.1

--- 9.1_callback function.js
+++ 9.1_callback function.js
@@ -1,9 +1,20 @@
-function isString(f) {
-    f();
-    f();
+const isString = (str, callback) => {
+    if (typeof str === "string")
+        callback(str)
 }
 
-function cry() {
-    console.log("im so sad");
-
+function callback(str) {
+    console.log(str);
 }
+// isString("wajde", callback)
+
+function firstworduppercase(str, dashWords) {
+    let firstword = str.split(' ')[0].toUpperCase();
+    str = firstword + str.slice(firstword.length, str.length)
+    dashWords(str)
+}
+const dashWords = (sentence) => {
+    console.log(sentence);
+    console.log(sentence.split(' ').join('-'));
+}
+firstworduppercase('im wajde hbaish', dashWords)
