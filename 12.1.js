1a75f1d74260fc462eee063904a7bc5a36535663
wajdehbaish <wajdehbaish@gmail.com>
2021-09-21 14:21:58 +0300

    12.1

--- 12.1.js
+++ 12.1.js
@@ -7,12 +7,20 @@ function mymaxfunc(nums) {
         return current
     })
 }
-console.log(mymaxfunc(numbers));
+// console.log(mymaxfunc(numbers));
 
 const array1 = [1, 2, 3, 4];
-const reducer = (previousValue, currentValue) => previousValue + currentValue;
-// console.log(array1.reduce(reducer));
+
+function reducer(arr) {
+    return arr.filter(ele => {
+        return (ele % 2 == 0 ? ele : 0)
+    }).reduce((pre, curr) => pre + curr)
+}
+
+
+// console.log(reducer(array1));
 
 function average(nums) {
     return nums.reduce((a, b) => (a + b)) / nums.length;
 }
+console.log(average(array1));
