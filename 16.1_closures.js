bd176e8d4598f514ed727dfb1d95a3090d418248
wajdehbaish <wajdehbaish@gmail.com>
2021-09-22 10:40:14 +0300

    16.1

---
+++ 16.1_closures.js
@@ -0,0 +1,40 @@
+// var b = 1;
+
+// function someFunction(number) {
+//     function otherFunction(input) {
+//         return b;
+//     }
+//     b = 5;
+//     return otherFunction;
+// }
+// var firstResult = someFunction(9);
+// var result = firstResult(2);
+
+
+// console.log(result);
+
+
+
+// 5
+
+// var a = 1;
+
+// function b2() {
+//     a = 10;
+//     return;
+
+//     function a() {}
+// }
+// b2();
+// console.log(a);
+
+// 1
+
+let i;
+for (i = 0; i < 3; i++) {
+    const log = () => {
+        console.log(i);
+    }
+    setTimeout(log, 100);
+}
+// 3 3 3
