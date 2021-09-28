1a80b14b23855c2be59fedca9ae20675eba33f22
wajdehbaish <wajdehbaish@gmail.com>
2021-09-28 12:53:12 +0300

    stop watch

--- stop watch/index.js
+++ stop watch/index.js
@@ -1,8 +1,8 @@
 const timer = document.getElementById('stopwatch');
 
-let hr = 0;
 let min = 0;
 let sec = 0;
+let ms = 0;
 let stoptime = true;
 
 function startTimer() {
@@ -20,46 +20,43 @@ function stopTimer() {
 
 function timerCycle() {
     if (stoptime == false) {
+        ms = parseInt(ms);
         sec = parseInt(sec);
         min = parseInt(min);
-        hr = parseInt(hr);
 
-        sec = sec + 1;
+        ms = ms + 1;
 
+        if (ms == 60) {
+            sec = sec + 1;
+            ms = 0;
+        }
         if (sec == 60) {
             min = min + 1;
             sec = 0;
-        }
-        if (min == 60) {
-            hr = hr + 1;
-            min = 0;
-            sec = 0;
+            ms = 0;
         }
 
+        if (ms < 10 || ms == 0) {
+            ms = '0' + ms;
+        }
         if (sec < 10 || sec == 0) {
             sec = '0' + sec;
         }
         if (min < 10 || min == 0) {
             min = '0' + min;
         }
-        if (hr < 10 || hr == 0) {
-            hr = '0' + hr;
-        }
 
-        timer.innerHTML = hr + ':' + min + ':' + sec;
+        timer.innerHTML = min + ':' + sec + ':' + ms;
 
-        setTimeout("timerCycle()", 1000);
+        setTimeout(timerCycle, 10);
     }
 }
 
-function resetTimer() {
-    timer.innerHTML = '00:00:00';
-}
 
 function resetTimer() {
     timer.innerHTML = "00:00:00";
     stoptime = true;
-    hr = 0;
-    sec = 0;
     min = 0;
+    ms = 0;
+    sec = 0;
 }
