6e0d3adbd5a1264251fa5794aeb68caca55bed14
wajdehbaish <wajdehbaish@gmail.com>
2021-10-05 12:06:24 +0300

    async/await

---
+++ async/await/index.js
@@ -0,0 +1,31 @@
+const getIDs = () =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([532, 543, 753, 1, 5]);
+        }, 1500);
+    });
+
+const getRecipe = (recipeID) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(
+            (ID) => {
+                const recipe = {
+                    title: "Fresh tomato pasta",
+                    publisher: "Pinchas Hodadad",
+                };
+                resolve(`${ID}: ${recipe.title}`);
+            },
+            1500,
+            recipeID
+        );
+    });
+};
+async function run() {
+    const idsarr = await getIDs()
+    console.log(idsarr);
+
+    const rescipe = await getRecipe(idsarr[2])
+    console.log(rescipe);
+}
+
+run()
